617de12e44995b9495cb09e776da0f9e40b84c2e
Odirlei Borgert <odirleiborgert@gmail.com>
2017-10-03 09:34:41 -0300

    First commit

---
+++ tests/Message/TextTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CodeBot\Message;
+
+use PHPUnit\Framework\TestCase;
+
+class TextTest extends TestCase {
+
+	public function testReturnToArray()
+	{
+		$actual = (new Text(1))->message('oi');
+		$expected = [
+			'recipient' => [
+				'id' => 1
+			],
+			'message' => [
+				'text' => 'oi',
+				'metadata' => 'DEVELOPER_DEFINED_METADATA'
+			]
+		];
+
+		$this->assertEquals($actual, $expected);
+	}
+	
+}
