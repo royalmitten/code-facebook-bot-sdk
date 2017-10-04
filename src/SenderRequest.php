31e1071aaded85bff8b817cf032ce4848fb3227a
Odirlei Borgert <odirleiborgert@gmail.com>
2017-10-04 10:53:22 -0300

    Class web hook

---
+++ src/SenderRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CodeBot;
+
+class SenderRequest {
+
+	public function __construct(string $pageAccessToken)
+	{
+		$event = file_get_contents("php://input");
+		$event = json_decode($event, true, 512, JSON_BIGINT_AS_STRING);
+		$this->event = $event['entry'][0]['messaging'][0];
+	}
+
+	public function getSenderId()
+	{
+		return $this->event['sender']['id'] ?? null;
+	}
+
+	public function getMessage()
+	{
+		return $this->event['message']['test'] ?? null;
+	}
+
+	public function getPostback()
+	{
+		if (empty($this->event['postback'])) {
+			return null;
+		}
+		if (is_array($this->event['postback']) and !empty($this->event['postback']['payload'])) {
+			$this->event['postback']['payload'];
+		}
+		return $this->event['postback'] ?? null;
+	}
+
+}
